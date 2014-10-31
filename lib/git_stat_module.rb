cf4879a6d87bea5dcca44123810ed76d6fe3edc2
sahellebusch <sahellebusch@gmail.com>
2014-10-30 22:03:51 -0500

    added get repos, not finished

--- lib/git_stat_module.rb
+++ lib/git_stat_module.rb
@@ -2,14 +2,19 @@ module GitStatsModule
 	require 'english'
 	require 'highline/import'
 	require 'octokit'
+	require 'awesome_print'
+	require 'json'
 
-	LOGIN_FAILURE_MSG = "Authentication failed."
+	USER_AUTHENTICATION_ERROR 	 = "Authentication failed."
+	GET_REPO_FAILURE_MSG = "An error occurred while trying to get repos. Gracefully exiting."
 
+	# Prompts for and retrieves Github username
 	def get_username
 		# TODO find out real username requirements
 		username = ask("Github login: ") { |un| un.validate = /^[\w]{4,20}$/ }
 	end
 
+	# Prompts for and retrieves Github password
 	def get_user_password
 		#TODO find out real password requirements
 		password = ask("Enter your password:  ") { |pw|
@@ -18,12 +23,34 @@ module GitStatsModule
 		}
 	end
 
-	def authorize_create_user(username, password)
-		client = Octokit::Client.new(:login => username, :password => password)
-		begin
-			client.user
-		rescue Exception => e
-			abort LOGIN_FAILURE_MSG
+	# Calls user.user to validate users credentials
+	# Params:
+	# +username+:: user provided Github username
+	# +password+:: user provided Github password
+	def authorize_get_user(username, password)
+		user = Octokit::Client.new(:login => username, :password => password)
+		if (user.user)
+			return user
+		else
+			abort USER_AUTHENTICATION_ERROR
 		end
 	end
+
+	# Retrieves list of repository names
+	# Params:
+	# +user+:: the Github user
+	def get_repos(user)
+		begin
+			repos = Array.new
+			response = user.repositories
+			# return an array of hashes, get just the names
+			response.each { |repo|
+				repos.push(repo["name"])
+			}
+			return repos
+		rescue Exception => e
+			abort GET_REPO_FAILURE_MSG
+		end
+	end
+
 end
--- lib/git_stat_tool.rb
+++ lib/git_stat_tool.rb
@@ -7,8 +7,13 @@ class GitStatTool
 	def initialize
 		username = GitStatTool.get_username
 		password = GitStatTool.get_user_password
-		@user 	 = GitStatTool.authorize_create_user(username, password)
-		puts @user.class
+		@user 	 = GitStatTool.authorize_get_user(username, password)
+		go
+	end
+
+	def go
+		options = GitStatTool.get_repos(@user)
+		ap options
 	end
 end
 
