b99ba804de2c7297d7f13396e3653391fd7b8938
sahellebusch <sahellebusch@gmail.com>
2014-11-01 17:22:10 -0500

    removed comments

--- git_tool.rb
+++ git_tool.rb
@@ -1,21 +1,12 @@
 #!/usr/bin/env ruby
 
-require_relative 'lib/git_stat_tool'
-
-#!/usr/bin/env ruby
 require 'optparse'
 require 'pp'
+require_relative 'lib/git_stat_tool'
 
-# This hash will hold all of the options
-# parsed from the command-line by
-# OptionParser.
 options = {}
 
 optparse = OptionParser.new do|opts|
-	# TODO: Put command-line options here
-
-	# This displays the help screen, all programs are
-	# assumed to have this option.
 	opts.on( '-h', '--help', 'Display this screen' ) do
 		puts opts
 		exit
@@ -30,12 +21,5 @@ optparse = OptionParser.new do|opts|
 	end
 end
 
-# Parse the command-line. Remember there are two forms
-# of the parse method. The 'parse' method simply parses
-# ARGV, while the 'parse!' method parses ARGV and removes
-# any options found there, as well as any parameters for
-# the options. What's left is the list of files to resize.
 optparse.parse!
-
-
 GitStatTool.new.go(options)
